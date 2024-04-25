bb1a38d4a64b1cbf0ac02186a7cbd58a242b3fe9
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-24 22:30:21 -0300

    Comonentes adicionados e código feito em sala feito

---
+++ my-app/src/app/animal-item/animal-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { Animal } from '../models/animal';
+
+@Component({
+  selector: 'app-animal-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-item.component.html',
+  styleUrl: './animal-item.component.css'
+})
+export class AnimalItemComponent {
+  /**
+   *
+   */
+  constructor() {
+    // this.animal = new Animal();
+    // this.animal.nome = "Mingau";
+    // this.animal.especie = "Felino";
+    // this.animal.idade = 18;
+    // this.animal.sexo = "Macho";
+
+  }
+
+  @Input() animal: Animal = new Animal("", "", 0, "");
+
+}
